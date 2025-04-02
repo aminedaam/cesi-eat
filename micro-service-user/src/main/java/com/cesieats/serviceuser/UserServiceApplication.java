d246d85ee0f5f19b896d40d597fe94224cb7fec4
amdaamouch <amine.daamouch2@soprabanking.com>
2025-04-02 13:53:02 +0200

    feat(API-GATEWAY) : ajout de la gateway + server eureka et modif coté service-user

--- micro-service-user/src/main/java/com/cesieats/serviceuser/UserServiceApplication.java
+++ micro-service-user/src/main/java/com/cesieats/serviceuser/UserServiceApplication.java
@@ -1,6 +1,7 @@
 package com.cesieats.serviceuser;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
